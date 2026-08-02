295b600be70493d5814dfd1242a92e8a5562e678
CordeiroGusta <guscordeiro199@gmail.com>
2026-08-02 17:29:55 -0300

    Metodo PUT criado com sucesso, ainda precisa fazer alguma alterações, mas, esta funcionando

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import users
+
+class Usuario(BaseModel):
+    id: int
+    usuario: str
+    email: str
+    telefone: str
+    ativo: bool
 
 #Objetivo: Crud de usuários
 #Url: vai ser gerada pelo Uvicorn
@@ -13,13 +21,17 @@ import users
 
 app = FastAPI()
 
-#GET geral
+#GET geral, exibe todos os usuarios
 @app.get('/users')
 def get_user():
     return users.listar_usuarios()
 
+#GET por ID, exibe apenas o usuario do ID informado
 @app.get('/users/{id}')
 def get_user_por_id(id: int):
     return users.listar_usuarios_id(id)
 
-@app.post('/users')
+#PUT por ID, atualiza os dados de um usuario especifico
+@app.put('/users/{id}')
+def put_atualizar_user (id: int, usuario: Usuario):
+    return users.atualizar_usuario(id, usuario.model_dump())
--- users.py
+++ users.py
@@ -4,33 +4,45 @@ usuarios = [
         "id": 1,
         "usuario": "fulano",
         "email": "fulanonice@sememail.com",
-        "telefone": "4112345678"
+        "telefone": "4112345678",
+        "ativo": True
     },
     {
         "id": 2,
         "usuario": "ciclano",
         "email": "bacana@sememail.com",
-        "telefone": "4187654321"
+        "telefone": "4187654321",
+        "ativo": True
     },
     {
         "id": 3,
         "usuario": "billy",
         "email": "dev@sememail.com",
-        "telefone": "4111223344"
+        "telefone": "4111223344",
+        "ativo": True
     },
     {
         "id": 4,
         "usuario": "roberts",
         "email": "dataengineer@sememail.com",
-        "telefone": "4188776655"
+        "telefone": "4188776655",
+        "ativo": False
     }
 ]
 
 #metodos
 def listar_usuarios():
+    '''Lista todos os usuários da base de dados'''
     return usuarios
 
 def listar_usuarios_id(id):
+    '''Lista o usuario que corresponder ao id fornecido'''
     for usuario in usuarios:
         if usuario['id'] == id:
             return usuario
+
+def atualizar_usuario(id, usuario_atualizado):
+    '''Atualiza os dados de um usuario que corresponder ao id fornecido'''
+    for indice, usuario in enumerate(usuarios):
+        if usuario['id'] == id:
+            usuarios[indice] = usuario_atualizado
