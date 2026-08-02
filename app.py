4e1fa302b8a4548e28c3e9914de4cdf6f3f6bb55
CordeiroGusta <guscordeiro199@gmail.com>
2026-08-02 18:18:02 -0300

    Iniciando o desenvolvimento do metodo POST

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ class Usuario(BaseModel):
 #Url: vai ser gerada pelo Uvicorn
 #Endpoints: {
 #    URL\users (GET),
-#    URL\user\{id} (GET),
+#    URL\users\{id} (GET),
+#    URL\users\{id} (PUT)
 #    URL\users\{id} (POST),
 #    URL\users\{id} (DELETE)
 # }
@@ -21,17 +22,19 @@ class Usuario(BaseModel):
 
 app = FastAPI()
 
-#GET geral, exibe todos os usuarios
+#GET, geral, exibe todos os usuarios
 @app.get('/users')
 def get_user():
     return users.listar_usuarios()
 
-#GET por ID, exibe apenas o usuario do ID informado
+#GET, por ID, exibe apenas o usuario do ID informado
 @app.get('/users/{id}')
 def get_user_por_id(id: int):
     return users.listar_usuarios_id(id)
 
-#PUT por ID, atualiza os dados de um usuario especifico
+#PUT, por ID, atualiza os dados de um usuario especifico
 @app.put('/users/{id}')
 def put_atualizar_user (id: int, usuario: Usuario):
     return users.atualizar_usuario(id, usuario.model_dump())
+
+#POST, por ID, cadastra um usuario novo
