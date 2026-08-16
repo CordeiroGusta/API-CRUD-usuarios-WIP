72d6de4edb6bf44988b139208cfda4e69c84d53b
CordeiroGusta <guscordeiro199@gmail.com>
2026-08-15 22:07:43 -0300

    refactor: ajuste em como é tratado a resposta de uma requisição e atualizado os endpoints

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
 import users
 
@@ -11,22 +11,27 @@ class Usuario(BaseModel):
 
 app = FastAPI()
 
-@app.get('/users')
+@app.get('/api/v1/users')
 def get_user():
-    return users.listar_usuarios()
+    usuarios = users.listar_usuarios()
+    return usuarios
 
-@app.get('/users/{id}')
+@app.get('/api/v1/users/{id}')
 def get_user_por_id(id: int):
-    return users.listar_usuarios_id(id)
+    usuario = users.listar_usuarios_id(id)
+    return usuario
 
-@app.put('/users/{id}')
+@app.put('/api/v1/users/{id}')
 def put_atualizar_user (id: int, usuario: Usuario):
-    return users.atualizar_usuario(id, usuario.model_dump())
+    usuario_atualizado = users.atualizar_usuario(id, usuario.model_dump())
+    return usuario_atualizado
 
-@app.post('/users')
+@app.post('/api/v1/users')
 def post_novo_user(usuario: Usuario):
-    return users.criar_usuario(usuario.model_dump())
+    novo_usuario = users.criar_usuario(usuario.model_dump())
+    return novo_usuario
 
-@app.delete('/users/{id}')
+@app.delete('/api/v1/users/{id}')
 def delete_apagar_user(id: int):
-    return users.deletar_usuario(id)
+    usuario_deletado = users.deletar_usuario(id)
+    return usuario_deletado
