a208835a3d9bf8a904d7f777695b730aedec4bf6
CordeiroGusta <guscordeiro199@gmail.com>
2026-08-18 20:26:32 -0300

    fix/feat: ajustado a resposta HTTP do metodo DELETE, e implementando assincronização e outras respostas HTTP nos demais metodos

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from fastapi import FastAPI,HTTPException
-from schemas.user_schema import criarUsuario, atualizarUsuario, Usuario
+from schemas.user_schema import atualizarUsuario, Usuario
 import users
 
 app = FastAPI()
 
 @app.get('/api/v1/users')
-def get_user():
+async def get_user():
     usuarios = users.listar_usuarios()
     return usuarios
 
 @app.get('/api/v1/users/{id}')
-def get_user_por_id(id: int):
+async def get_user_por_id(id: int):
     usuario = users.listar_usuarios_id(id)
     if usuario == None:
         raise HTTPException(
@@ -24,20 +24,10 @@ def get_user_por_id(id: int):
     return usuario
 
 @app.put('/api/v1/users/{id}')
-def put_atualizar_user (id: int, usuario: atualizarUsuario):
+async def put_atualizar_user (id: int, usuario: atualizarUsuario):
     dados = usuario.model_dump(exclude_unset=True)
     usuario_atualizado = users.atualizar_usuario(id, dados)
-    return usuario_atualizado
-
-@app.post('/api/v1/users')
-def post_novo_user(usuario: Usuario):
-    novo_usuario = users.criar_usuario(usuario.model_dump()) 
-    return novo_usuario
-
-@app.delete('/api/v1/users/{id}')
-def delete_apagar_user(id: int):
-    usuario_deletado = users.deletar_usuario(id)
-    if usuario_deletado == None:
+    if usuario_atualizado == None:
         raise HTTPException(
             status_code=404,
             detail= {
@@ -46,12 +36,23 @@ def delete_apagar_user(id: int):
                 "message": "Usuario não encontrado  " 
             }
         )
-    else:
+    return usuario_atualizado
+
+@app.post('/api/v1/users')
+async def post_novo_user(usuario: Usuario):
+    novo_usuario = users.criar_usuario(usuario.model_dump())
+    return novo_usuario #RESPONSE
+
+@app.delete('/api/v1/users/{id}')
+async def delete_apagar_user(id: int):
+    usuario_deletado = users.deletar_usuario(id)
+    if usuario_deletado == None:
         raise HTTPException(
-            status_code=204,
-            detail={
-                "status": 204,
-                "code": "NO_CONTENT",
-                "message": "Usuário apagado com sucesso"
+            status_code = 404,
+            detail = {
+                "status": 404,
+                "code": "USER_NOT_FOUND",
+                "message": "Usuario não encontrado  " 
             }
         )
+    return HTTPException (status_code = 204)
